f54ff053cae49411742282067b322148b2605544
yuvalkrin@gmail.com <yuval123654>
2020-04-23 18:52:02 +0300

    first update

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -10,7 +10,7 @@ Router.onRouteChangeError = () => NProgress.done();
 
 function Header() {
 
-  const user=false;
+  const user=true;
   const router = useRouter();
 
   function isActive(route) {
@@ -18,85 +18,92 @@ function Header() {
   }
   return (
       //<Segment ui inverted vertical centered aligned style={{minHeight: "350px"}}>
-      <Menu fluid id={"menu"} stackable inverted >
-        <Container text>
-          <Link href="/">
-          <Menu.Item header active={isActive("/")}>
-            <Image
-                size="mini"
-                src="/static/logo.svg"
-                style={{ marginRight: '1em' }}
-            />
-            Flower Shop
-          </Menu.Item>
-        </Link>
+      <Segment
+          inverted
+          textAlign='center'
+          style={{ minHeight: 400, padding: '1em 0em'}}
+          vertical
+          id={"menu"}
+      >
+        <Menu fluid stackable pointiong borderless style={{backgroundColor: 'transparent'}}>
+          <Container text>
+            <Link href="/">
+              <Menu.Item header active={isActive("/")}>
+                <Image
+                    size="mini"
+                    src="/static/logo.svg"
+                    style={{ marginRight: '1em' }}
+                />
+                Flower Shop
+              </Menu.Item>
+            </Link>
 
-          <Link href="/cart">
-            <Menu.Item header active={isActive("/cart")}>
-              <Icon
-                  name={"cart"}
-                  size={"large"}
-              />
-              My Cart
-            </Menu.Item>
-          </Link>
+            <Link href="/cart">
+              <Menu.Item header active={isActive("/cart")}>
+                <Icon
+                    name={"cart"}
+                    size={"large"}
+                />
+                My Cart
+              </Menu.Item>
+            </Link>
 
             {user &&
             <Link href="/create">
-                <Menu.Item header active={isActive("/create")}>
-                    <Icon
-                        name={"add square"}
-                        size={"large"}
-                    />
-                    Create
-                </Menu.Item>
+              <Menu.Item header active={isActive("/create")}>
+                <Icon
+                    name={"add square"}
+                    size={"large"}
+                />
+                Create
+              </Menu.Item>
             </Link>
             }
 
-          {user ? (<>
-          <Link href="/account">
-            <Menu.Item header active={isActive("/account")}>
-              <Icon
-                  name={"heart outline"}
-                  size={"large"}
-              />
-              My Account
-            </Menu.Item>
-          </Link>
+            {user ? (<>
+                  <Link href="/account">
+                    <Menu.Item header active={isActive("/account")}>
+                      <Icon
+                          name={"heart outline"}
+                          size={"large"}
+                      />
+                      My Account
+                    </Menu.Item>
+                  </Link>
+                  <Menu.Item header>
+                    <Icon
+                        name={"sign out"}
+                        size={"large"}
+                    />
+                    Logout
+                  </Menu.Item>
+                </>)
+                :(<>
+                  <Link href="/login">
+                    <Menu.Item header active={isActive("/login")}>
+                      <Icon
+                          name={"sign-in"}
+                          size={"large"}
+                      />
+                      Login
+                    </Menu.Item>
+                  </Link>
 
-            <Menu.Item header>
-              <Icon
-                  name={"sign out"}
-                  size={"large"}
-              />
-              Logout
-            </Menu.Item>
-              </>)
-          :(<>
-              <Link href="/login">
-                <Menu.Item header active={isActive("/login")}>
-                  <Icon
-                      name={"sign-in"}
-                      size={"large"}
-                  />
-                  Login
-                </Menu.Item>
-              </Link>
+                  <Link href="/signup">
+                    <Menu.Item header active={isActive("/signup")}>
+                      <Icon
+                          name={"signup"}
+                          size={"large"}
+                      />
+                      Signup
+                    </Menu.Item>
+                  </Link>
+                </>)
+            }
 
-              <Link href="/signup">
-                <Menu.Item header active={isActive("/signup")}>
-                  <Icon
-                    name={"signup"}
-                    size={"large"}
-                  />
-                  Signup
-                </Menu.Item>
-              </Link>
-            </>)
-          }
-
-        </Container>
-      </Menu>
+          </Container>
+        </Menu>
+      </Segment>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import axios from 'axios'
+import {Container, Divider, Segment} from "semantic-ui-react";
 import ProductList from '../components/Index/ProductList'
 import baseUrl from "../utils/baseUrl";
 
 function Home( {products} ) {
-   return <ProductList products={products} />;
+   return (
+       <>
+        <Segment
+           textAlign='center'
+           style={{ minHeight: 400, padding: '1em 0em'}}
+           vertical>
+            <Container text>
+                <Divider horizontal><h2>About Us</h2></Divider>
+                <h3>
+                Our crowns are made from fresh flowers especially for you.
+                </h3>
+                <h3>
+                Would fit best for your birthday, bachelorette party or any other special day of yours.
+                </h3>
+                <Divider/>
+            </Container>
+        </Segment>
+       <ProductList products={products} />
+       </>);
 }
 
 Home.getInitialProps = async () => {
